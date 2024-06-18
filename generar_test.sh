8bc75e03b91f42d94caf04d0e6f47c11f5385425
Raúl Gilabert Gámez <raulgilabert@proton.me>
2024-06-18 17:55:28 +0200

    fix error with sisa_testing_framework
    https://github.com/rdvdev2/sisa_testing_framework

--- generar_test.sh
+++ generar_test.sh
@@ -9,3 +9,7 @@ sisa-as ${filename}_translated.S -o ${filename}_translated.o
 
 sisa-objdump -d -z --section=.text ${filename}_translated.o > ${filename}_translated.code
 ./limpiar.pl codigo ${filename}_translated.code
+
+cat ${filename}_translated.code.rom | tr '[:lower:]' '[:upper:]' > ${filename}_translated.code.rom_upper
+
+mv ${filename}_translated.code.rom_upper ${filename}_translated.code.rom
